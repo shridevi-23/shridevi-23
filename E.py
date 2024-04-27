94511d98a03507b5c251c5f3832f95338674a2f1
shridevi-23 <167217131+shridevi-23@users.noreply.github.com>
2024-04-27 11:31:18 +0530

    Create E.py

---
+++ E.py
@@ -0,0 +1,12 @@
+def print_pattern_E(n):
+    for i in range(n):
+        for j in range(n):
+            if (i == 0 or i == n // 2 or i == n - 1) or (j == 0):
+                print("*", end="")
+            else:
+                print(" ", end="")
+        print()
+
+
+size = 7
+print_pattern_E(size)
